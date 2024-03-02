c2ecba83feba9418a6a447f9afd5919908443a82
johashhh <>
2024-03-02 19:46:57 +0800

    move database to AWS

--- server/database/__init__.py
+++ server/database/__init__.py
@@ -1,5 +1,12 @@
 import psycopg2
 
+host = 'my-db-instance.cnugoekc83o7.ap-southeast-2.rds.amazonaws.com'
+port = '5432'
+dbname = 'initial_db'
+user = 'postgres'
+password = '4i8YBjL-LC14'
+
+# (host=, dbname="postgres", user="postgres", password="a194369672", port=5433)
 
 class person:
     def __init__(self, id, name):
@@ -7,7 +14,7 @@ class person:
         self.name = name
     
     def CREATE_TABLE(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""
@@ -25,7 +32,7 @@ class person:
         conn.close()
     
     def DROP_TABLE(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""DROP TABLE IF EXISTS person;""")
@@ -36,8 +43,23 @@ class person:
         cur.close()
         conn.close()
     
+    def SHOW(self):
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
+        cur = conn.cursor()
+        try:
+            cur.execute("""SELECT * FROM person;""")
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+            print("SHOW_PERSON successful...\n")
+        except Exception as error:
+            print("SHOW_PERSON error...", error)
+        conn.commit()
+        cur.close()
+        conn.close()
+    
     def ADD(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""INSERT INTO person (name) VALUES (%s);""", (self.name,))
@@ -49,7 +71,7 @@ class person:
         conn.close()
     
     def DELETE(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""DELETE FROM person WHERE id = (%s) AND name = (%s);""", (self.id, self.name))
@@ -73,7 +95,7 @@ class message:
         self.timestamp = timestamp;
     
     def CREATE_TABLE(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""
@@ -92,7 +114,7 @@ class message:
         conn.close()
     
     def DROP_TABLE(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""DROP TABLE IF EXISTS message;""")
@@ -102,9 +124,24 @@ class message:
         conn.commit()
         cur.close()
         conn.close()
+
+    def SHOW(self):
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
+        cur = conn.cursor()
+        try:
+            cur.execute("""SELECT * FROM message;""")
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+            print("SHOW_MESSAGE successful...\n")
+        except Exception as error:
+            print("SHOW_MESSAGE error...", error)
+        conn.commit()
+        cur.close()
+        conn.close()
     
     def ADD(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""INSERT INTO message (content, timestamp) VALUES (%s, %s);""", (self.content, self.timestamp))
@@ -116,7 +153,7 @@ class message:
         conn.close()
 
     def DELETE(self):
-        conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres", password="a194369672", port=5433)
+        conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
         cur = conn.cursor()
         try:
             cur.execute("""DELETE FROM message WHERE id = (%s) AND content = (%s) AND timestamp = (%s);""", (self.id, self.content, self.timestamp))
--- server/test.py
+++ server/test.py
@@ -1,14 +1,16 @@
 import database
 import psycopg2
 
-p1 = database.person(1, "Jay")
-m1 = database.message(1, "Hi there!", "2024-02-29 12:50:00")
+# class person(SERIAL PRIMARY KEY id, VARCHAR(255) name)
+p1 = database.person(0, "Mai")
+p2 = database.person(0, "Jay")
+p3 = database.person(0, "Leo")
 
-p2 = database.person(3, "Kai")
-m2 = database.message(1, "Hello world!", "2024-02-29 12:00:00")
+# class message(SERIAL PRIMARY KEY id, VARCHAR(4096) content, TIMESTAMP timestamp)
+m1 = database.message(0, "Hello world!", "2024-02-29 12:00:00")
+m2 = database.message(0, "Hi there!", "2024-02-29 12:50:00")
+m3 = database.message(0, "How's everything going?", "2024-02-29 14:50:00")
 
-#p1.CREATE_TABLE()
-# p1.ADD()
-#p2.DELETE()
 
-m2.DELETE()
+p1.SHOW()
+m1.SHOW()
